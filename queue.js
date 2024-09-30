9cc32224b5a91d814cabe658a1c0d173ee802bb3
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-30 10:22:48 +0200

    Implement queue for the bfs algorithm

---
+++ queue.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+module.exports = class Queue {
+  #head;
+  #tail;
+
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (!this.#head) {
+      this.#head = node;
+      this.#tail = node;
+    } else {
+      this.#tail.next = node;
+      this.#tail = node;
+    }
+  }
+
+  dequeue() {
+    if (!this.#head) return null;
+
+    const dequeuedValue = this.#head.value;
+    this.#head = this.#head.next;
+
+    if (!this.#head) this.#tail = null;
+    return dequeuedValue;
+  }
+
+  isEmpty() {
+    return this.#head === null;
+  }
+};
